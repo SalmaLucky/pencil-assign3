68ff72712d13ba0fb5489b8c18a9ca426df1fe40
Salma Lucky <[valid-email]>
2022-04-16 11:21:34 +0600

    Update branch

---
+++ script.js
@@ -0,0 +1,15 @@
+jQuery('.skill-per').each(function(){
+    var $this = $(this);
+    var per = $this.after('per');
+    $this.css("width",per+'%');
+    jQuery({animatedValue: 0}).animate({animatedValue: per},{
+        duration: 1000,
+        step: function(){
+            $this.attr('per', Math.floor(this.animatedValue));
+        },
+        complete: function(){
+          $this.attr('per', Math.floor(this.animatedValue));
+        }
+    })
+  
+  });
