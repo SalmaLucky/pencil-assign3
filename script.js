f74a7b194c1aca085d6093a7def1f40e5e22c3c1
mahabubAlahi <mahabubalahi9531@gmail.com>
2022-05-15 00:52:15 +0600

    counterup feature added

--- script.js
+++ script.js
@@ -1,47 +1,52 @@
 /*progressbar*/
-$(document).ready(function(){
+$(document).ready(function () {
 
-  
-jQuery('.skill-per').each(function(){
+
+  jQuery('.skill-per').each(function () {
     var $this = $(this);
     var per = $this.data('per');
-    $this.css("width",per+'%');
-    jQuery({animatedValue: 0}).animate({animatedValue: per},{
-        duration: 1000,
-        step: function(){
-            $this.attr('per', Math.floor(this.animatedValue));
-        },
-        complete: function(){
-          $this.attr('per', Math.floor(this.animatedValue));
-        }
+    $this.css("width", per + '%');
+    jQuery({ animatedValue: 0 }).animate({ animatedValue: per }, {
+      duration: 1000,
+      step: function () {
+        $this.attr('per', Math.floor(this.animatedValue));
+      },
+      complete: function () {
+        $this.attr('per', Math.floor(this.animatedValue));
+      }
     })
-  
+
   });
-  
+
   /*gallery*/
-  
-    
-    jQuery('.category').click(function(){
-     
 
-      //jQuery(this).addClass('active').siblings().removeClass('active');
-      //jQuery(this).addClass("active");
-      const value = jQuery(this).attr('data-filter'); 
-      if (value == 'full-gallery'){
-        jQuery('.image').show('1000');
-      }
-      else{
-        jQuery('.image').not('.'+value).hide('1000');
-        jQuery('.image').filter('.'+value).show('1000');
-      }
-      jQuery('.category').click(function(){
-        jQuery(this).addClass('active').siblings().removeClass('active');
-      });
+
+  jQuery('.category').click(function () {
+
+
+    //jQuery(this).addClass('active').siblings().removeClass('active');
+    //jQuery(this).addClass("active");
+    const value = jQuery(this).attr('data-filter');
+    if (value == 'full-gallery') {
+      jQuery('.image').show('1000');
+    }
+    else {
+      jQuery('.image').not('.' + value).hide('1000');
+      jQuery('.image').filter('.' + value).show('1000');
+    }
+    jQuery('.category').click(function () {
+      jQuery(this).addClass('active').siblings().removeClass('active');
     });
+  });
 
 
- // jQuery methods go here...
- //counter
- 
+  // jQuery methods go here...
+  //counter
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+  });
+
 
 });
